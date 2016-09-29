944104b82a500b4bfc901dd3bbf4de137dd2d3e3
claire <cho00045@algonquinlive.com>
2016-09-29 09:58:35 -0400

    Finished Lab3

--- app/src/main/java/com/example/clear/lab1/ListItemsActivity.java
+++ app/src/main/java/com/example/clear/lab1/ListItemsActivity.java
@@ -1,13 +1,137 @@
 package com.example.clear.lab1;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton); // reference
+        Switch switch1 = (Switch) findViewById(R.id.switch1); // reference
+        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox); // reference
+
+        /* */
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+
+        });
+
+        /* #8 Swith */
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                CharSequence text;// "Switch is Off"
+                int duration;
+                if (isChecked) {
+                    text = "Switch is On";
+                    duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+                } else {
+                    text = "Switch is Off";
+                    duration = Toast.LENGTH_LONG; //= Toast.LENGTH_LONG if Off
+                }
+
+                Toast toast = Toast.makeText(ListItemsActivity.this, text, duration); //this is the ListActivity
+                toast.show(); //display your message box
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                        .setTitle(R.string.dialog_title)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                                Intent resultIntent = new Intent();
+                                resultIntent.putExtra("Response", "My information to share");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+            }
+        });
     }
+
+    /* get thumnail */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
 }
--- app/src/main/java/com/example/clear/lab1/LoginActivity.java
+++ app/src/main/java/com/example/clear/lab1/LoginActivity.java
@@ -1,29 +1,121 @@
 package com.example.clear.lab1;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editText;
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        /* #4-2
+        * Add a login button to the LoginActivity, and store a reference to the button in the Java Activity.
+        */
+        Button loginBt = (Button) findViewById(R.id.button2); // reference
+
+        /* #4-3
+        * Create a callback function for the login button so that it stores the text
+        * in the login email field to the Shared Preferences of your application.
+        * Don’t forget to commit the changes!
+        * Next, call startActivity() with your Lab1 StartActivity as the next Activity to start:
+        * Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+        * startActivity(intent);
+        */
+        loginBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences shared = getSharedPreferences("email", MODE_PRIVATE);
+                SharedPreferences.Editor editor = shared.edit();
+                editor.putString("DefaultEmail", editText.getText().toString());
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent); // explicitly defines the activity you want to start, using the class name
+
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        Log.i(ACTIVITY_NAME, "In onStart()");
+
+        /* #4-3
+         * Modify the onStart() function to read the value of the stored email address
+         * in SharedPreferences and sets the initial text of the login email field.
+         * The first time you run your application and read from the preferences,
+         * the value saved under your email string will be missing,
+         * so your sharedPreferences.getString(“DefaultEmail”, “email@domain.com”)
+         * call will make use of the default value parameter (2nd parameter).
+         */
+        SharedPreferences sharedPreferences = getSharedPreferences("email", MODE_PRIVATE);
+        String defaultValue = sharedPreferences.getString("DefaultEmail","happy@domain.com");
+        editText.setText(defaultValue);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(ACTIVITY_NAME, "In onPause()");
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(ACTIVITY_NAME, "In onStop()");
     }
 
 }
